1ca0840b8ec287b42297a47992c2cc45bf2191ac
saif <santhoshi.batchu@yitsol.com>
2017-12-29 01:31:22 +0530

    git commit

---
+++ http-example1.js
@@ -0,0 +1,9 @@
+var http = require("http");
+
+http.createServer(function(req,res){
+
+    res.writeHead(200,{'content-type':'text/html'});
+    res.write(req.url);
+    res.end();
+
+}).listen(8080);
